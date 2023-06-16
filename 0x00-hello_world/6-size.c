1466c327639de21701293beca4096997bf9a0c41
Uncletbuddy <ochonmathankgod@gmail.com>
2023-06-15 23:06:56 -0700

    progress

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main-Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char.%d byte(s)\n",
+sizeof(char));
+	printf("Size of an init: %d byte(s)\n",
+sizeof(init));
+	printf("Size of a long init: %d bytes(s)\n",
+sizeof(long int));
+	printf("Size of a long long init: %d
+byte(s)\n", sizeof(long long int));
+	Printf('Size of a float: %d byte(s)\n"
+sizeof(float));
+	return (0);
+}
