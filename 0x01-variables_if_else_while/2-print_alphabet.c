69d899c3af41be2f8fb64e18855cf605f42a721e
Uncletbuddy <ochonmathankgod@gmail.com>
2023-06-17 13:28:49 -0700

    last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/** 
+ * int main - Entry point
+ * Description: Alphabet on smakk leters
+ * Return (0)
+ */
+
+int main
+{
+	init n  = a;
+
+	while(n <= z)
+	putchar ("n\n");
+	n++
+	
+	putchar("\n");
+
+	return (0)
+}
