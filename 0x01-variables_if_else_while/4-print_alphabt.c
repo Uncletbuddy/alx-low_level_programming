d77646bb75adf8d4832de2f5b24cacbe25b76850
Uncletbuddy <ochonmathankgod@gmail.com>
2023-06-17 15:41:14 -0700

    everyone can try

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: ommision of letters
+ * Return: (0)
+ */
+
+int main(void)
+{
+	char n;
+
+	for
+	(n = 'a';
+	n <= 'z';
+	n++);
+	if
+	(n != 'q');
+	(n != 'e');
+	{
+	putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
