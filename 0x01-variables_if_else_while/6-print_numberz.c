ac8b9aab0817162ad0505db0117c5cdf851560ec
Uncletbuddy <ochonmathankgod@gmail.com>
2023-06-19 16:11:58 -0700

    print number with putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,10 +1,19 @@
-#include <stdio>
+#include <stdio.h>
 /**
  * main - Entry point
  * Description:
  * Return: 0
  */
 
-int main ()
+int main(void)
 {
+	int e = 48;
 
+	while (e <= 57)
+	{
+		putchar(e);
+		e++;
+	}
+	putchar('\n');
+	return (0);
+}
