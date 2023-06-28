aacb479a18806a9a214be2b97613486478f1d3bb
Uncletbuddy <ochonmathankgod@gmail.com>
2023-06-28 05:07:55 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int _strlen - length of a string
+ *
+ * Return: always 0.
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+	while (*s != '0')
+	{
+		length++;
+		s++;	
+	}
+
+	return length;
+}
