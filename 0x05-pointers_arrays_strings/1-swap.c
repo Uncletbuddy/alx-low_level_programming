3fa8ccc881c55b2b5e674875fbb416e1479ed3bd
Uncletbuddy <ochonmathankgod@gmail.com>
2023-06-27 17:13:33 -0700

    swap values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int: swapping two file
+ * @a: first parameter
+ * @b: second parameter
+ * 
+ * Return: always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	a = 42
+	b = 98
+}
