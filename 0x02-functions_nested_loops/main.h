0d0d2cd35ba1047b0e66a6a491df64dd1ce2fba1
Uncletbuddy <ochonmathankgod@gmail.com>
2023-06-29 23:25:14 -0700

    print alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,18 @@
 #define MAIN_H
 
 int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int print_sign(int n);
+int _isalpha(int c);
+int _islower(int c);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+
+
 
 #endif /* MAIN_H */
 
