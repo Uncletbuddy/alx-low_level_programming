a98fa7499627fb32174b2c63664ca1de059ea2ff
Uncletbuddy <ochonmathankgod@gmail.com>
2023-07-02 12:11:52 -0700

    The standard library provides a similar function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - fuctions that computes the absolute value of an integer
+ *@r: the value
+ * Description: The standard library provides a similar function
+ *
+ * Return: (0)
+ *
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+
+		return (r);
+	else
+		return (r * -1);
+
+}
